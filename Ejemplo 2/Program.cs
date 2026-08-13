a05e56768b1fd8f0a6cafc583abd713daa732127
Willy Phelan <phelanguille@gmail.com>
2026-08-13 19:22:42 -0300

    Add project files.

---
+++ Ejemplo 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_2 {
+    internal class Program {
+        static void Main(string[] args){
+
+        }
+    }
+}
