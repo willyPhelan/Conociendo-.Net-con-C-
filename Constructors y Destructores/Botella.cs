e6d1f3e514baee9bd9eb14bb1390f6a9ed1d7718
Willy Phelan <phelanguille@gmail.com>
2026-08-16 11:18:34 -0300

    Septimo Commit Constructores y Destructores

---
+++ Constructors y Destructores/Botella.cs
@@ -0,0 +1,41 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_7_Clases{
+    internal class Botella{
+
+        public Botella(string color, string material){
+           
+            this.color = color ;
+
+            this.material = material ;
+        }
+
+        // sobre carga de constructores
+
+        public Botella(string color, string material, int capacidad){
+
+            this.color = color ;
+            this.material = material ;
+            this.capacidad = capacidad ; 
+        }
+
+        // constructor por defecto
+
+        public Botella() {} 
+
+        private int capacidad ; 
+        private string color ;
+        private string material ;
+
+        // Propidades 
+
+        public int Capacidad { get => capacidad ; set => capacidad = value ; } // modificable
+        public string Color { get => color ; } // read only
+        public string Material { get => material ; } // read only
+
+    }
+}
---
+++ Constructors y Destructores/Persona.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_7_Clases {
+    internal class Persona {
+
+    private int edad ; 
+    private float sueldo ; 
+
+    private string nombre ;  
+
+    public void setEdad(int edad) { // metodo 
+
+        this.edad = edad ;
+    }
+
+    public int getEdad() {
+
+            return this.edad ;
+        } 
+
+        }}
